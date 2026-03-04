3872671fb73fe92d461784f70618fb4f37f31ba1
Tres Douglas <tresdoug@gmail.com>
2026-03-04 03:57:23 +0000

    maybe?

--- affine.cpp
+++ affine.cpp
@@ -209,7 +209,7 @@ Matrix transformMenu()
                 result = translate(x, y) * result;
                 break;
             case 'R':
-                cout << "Enter the angle of rotation (degrees) ";
+                cout << "Enter the angle of rotation (degrees): ";
                 cin >> angle;
                 result = transRotate(angle) * result;
                 break;
